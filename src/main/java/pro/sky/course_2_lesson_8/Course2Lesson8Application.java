42f319423455d2697a9059bb768c25a0f8bb83f6
Alexey <lexshadrin@mail.ru>
2022-07-07 10:54:15 +1100

    Initial commit

---
+++ src/main/java/pro/sky/course_2_lesson_8/Course2Lesson8Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_lesson_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson8Application.class, args);
+    }
+
+}
